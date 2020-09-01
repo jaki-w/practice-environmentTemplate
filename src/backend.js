338979229fa2b8ce5f7f2dbd8185acbb23fd1806
Jaki Widby <jaki.widby@gmail.com>
2020-08-31 17:09:56 -0700

    add generic constructor and prototype

---
+++ src/backend.js
@@ -0,0 +1,8 @@
+function TheConstructor(parameter, parameter2) {
+  this.parameter = parameter;
+  this.parameter2 = parameter2;
+}
+
+TheConstructor.prototype.doSomething = function() {
+  return "I will do something soon...probably."
+}
